0a99eb53ab2128572efca16f39b6e9a7cc7aab94
Jennings Zhang <jenni_zh@protonmail.com>
2018-01-17 17:37:52 -0500

    New user server logic

--- server/server.js
+++ server/server.js
@@ -5,28 +5,12 @@ const app = express();
 app.use(require('cookie-parser')());
 const bodyParser = require('body-parser').urlencoded({ extended: false });
 
-app.post('/login', bodyParser, function (req, res) {
-    if (!req.body) return res.sendStatus(400);
-    res.send(req.body);
-});
-
-app.post('/newuser', bodyParser, function (req, res) {
-    if (!req.body) return res.sendStatus(400);
-    res.send(req.body);
-});
-
-
 app.use('/', express.static('home'));
 app.use('/assets', express.static('assets'));
 app.use('/vendor', express.static('vendor'));
 app.use('/vendor', express.static('node_modules/materialize-css/dist/js'));
 app.use('/vendor', express.static('node_modules/material-design-icons/iconfont'));
 app.use('/vendor', express.static('node_modules/jquery/dist'));
-
-app.use(function (req, res) {
-    res.status(404).end();
-    output(req, colors.dim('not found'), res.statusCode);
-});
 
 // connect to MySQL server
 (function () {
@@ -43,6 +27,20 @@ app.use(function (req, res) {
             console.error(colors.bgRed('Cannot connect to MySQL server.'.bold.white));
             console.dir(sql_config);
             console.log(err);
+            app.post('/login', bodyParser, function (req, res) {
+                if (!req.body) return res.sendStatus(400);
+                res.send(req.body);
+            });
+
+            app.post('/newuser', bodyParser, function (req, res) {
+                if (!req.body) return res.sendStatus(400);
+                res.send(req.body);
+            });
+
+            app.use(function (req, res) {
+                res.status(404).end();
+                output(req, colors.dim('not found'), res.statusCode);
+            });
         }
         else
             sqlOK(mysql);
@@ -59,14 +57,56 @@ console.log(`Server running at ${colors.underline(url)}`);
  */
 function sqlOK(mysql) {
     console.log(colors.bold(`Connected to ${colors.rainbow('MySQL!')}`));
+    var nextSession = 1;
+    var allSessions = new Map();
 
-    let test = 'SELECT 1 + 1 AS solution;';
-    mysql.query(test, function (err, rows, fields) {
-        if (err) throw err;
-        console.log(colors.dim(`${test} rows[0].solution --> ${rows[0].solution} ${rows[0].solution == 2 ? 'PASS' : 'FAIL'}`));
+    app.post('/login', bodyParser, function (req, res) {
+        if (!req.body) return res.sendStatus(400);
+        res.send(req.body);
+    });
+
+    /*
+     * Passwords are received unencrypted, they are stored unhashed.
+     * The SQL query is being made with a 'prepared statement', meaning this form is immune to SQLi.
+     */
+    app.post('/newuser', bodyParser, function (req, res) {
+        if (!req.body || !req.body.name || !req.body.pass) return res.sendStatus(400);
+
+        // add new user to database
+        mysql.query('INSERT INTO People (Username, Pass) VALUES (?, ?)',
+            [req.body.name, req.body.pass], function (error, results, fields) {
+                if (error) {
+                    switch (error.code) {
+                        case 'ER_DUP_ENTRY':
+                            res.cookie('message', { title: error.code, content: 'User already exists.' });
+                            res.sendFile('cookie_check.html', { root: `${__dirname}/home/` });
+                            break;
+                        case 'ER_DATA_TOO_LONG':
+                            res.sendStatus(400);
+                            break;
+                        default:
+                            res.sendCode(503);
+                    }
+                    dbOutput(`name=${req.body.name}`, colors.red(error.code));
+                }
+                else {
+                    allSessions.set(nextSession, req.body.name);
+                    res.cookie('sessionID', nextSession);
+                    res.sendFile('cookie_check.html', { root: `${__dirname}/home/` });
+                    dbOutput(`name=${req.body.name} and sessionID=${nextSession++}`, colors.green('user created :)'));
+                }
+            });
+
+        const dbOutput = (param, result) => {
+            console.log(`${colors.dim('SQL:')} ${colors.bold(param)} ${result}`)
+        }
+    });
+
+    app.use(function (req, res) {
+        res.status(404).end();
+        output(req, colors.dim('not found'), res.statusCode);
     });
 }
-
 
 // output(req, `message`, res.statusCode);
 function output(req, info, statusCode) {
